56f1867a243332b460046e6ced9661cacb6a9288
saberghost <saberghost@outlook.com>
2018-10-24 00:55:30 +0800

    拷贝目录

--- ComicCrop/Program.cs
+++ ComicCrop/Program.cs
@@ -2,6 +2,7 @@
 using System.IO;
 using System.Threading;
 using System.Threading.Tasks;
+using System.Linq;
 using SixLabors.ImageSharp;
 using SixLabors.ImageSharp.Formats.Jpeg;
 using SixLabors.ImageSharp.PixelFormats;
@@ -12,14 +13,22 @@ namespace ComicCrop
 {
     class Program
     {
+        public static string TarDir { get; set; }
+        public static string CurDir { get; set; }
+
         static void Main(string[] args)
         {
+            TarDir = @"E:\Comic";
+            CurDir = @"D:\Comic";
+            FindFile(CurDir);
+            return;
+
             TaskFactory tf = new TaskFactory();
             string[] filePaths = Directory.GetFiles(@"D:\新建文件夹");
-            Task[] tasks=new Task[filePaths.Length];
+            Task[] tasks = new Task[filePaths.Length];
             for (int i = 0; i < filePaths.Length; i++)
             {
-                tasks[i]= tf.StartNew(Convert,filePaths[i]);
+                tasks[i] = tf.StartNew(Convert, filePaths[i]);
             }
 
             Task.WaitAll(tasks);
@@ -29,7 +38,7 @@ namespace ComicCrop
 
         public static void Convert(object state)
         {
-            string filePath=state.ToString();
+            string filePath = state.ToString();
             string fileName = Path.GetFileNameWithoutExtension(filePath);
             string fileExt = Path.GetExtension(filePath);
             fileExt = ".jpg";
@@ -54,6 +63,28 @@ namespace ComicCrop
                     imgRight.Save($@"D:\output\{fileName}_2{fileExt}");
                 };
             }
+        }
+
+        public static void FindFile(string path)
+        {
+            DirectoryInfo dirInfo = new DirectoryInfo(path);
+            DirectoryInfo[] dirInfos = dirInfo.GetDirectories();
+            dirInfos.ToList().ForEach(s =>
+            {
+                string dir1 = s.FullName.Replace(CurDir, "").TrimStart('\\');
+                string dir2 = Path.Combine(TarDir, dir1);
+                DirectoryInfo info2 = new DirectoryInfo(dir2);
+                if (!info2.Exists)
+                {
+                    info2.Create();
+                }
+            });
+
+            if (dirInfos.Length == 0)
+            {
+                return;
+            }
+            dirInfos.ToList().ForEach(s => FindFile(s.FullName));
         }
     }
 }
